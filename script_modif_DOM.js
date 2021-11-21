c12ebffc3374ee9d797cfc56bddda782c5f3fb31
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-11-21 10:20:17 +0100

    2 last questions remaining

--- script_modif_DOM.js
+++ script_modif_DOM.js
@@ -29,3 +29,42 @@ function changeLogoName() {
 }
 changeLogoName();
 
+// Modification 4.
+let imagesArray = ["https://img.icons8.com/color/480/000000/html-5.png", "https://img.icons8.com/color/480/000000/css3.png", "https://img.icons8.com/color/480/000000/javascript.png", "https://img.icons8.com/color/480/000000/ruby-programming-language.png", "https://img.icons8.com/color/480/000000/bootstrap.png", "https://img.icons8.com/color/480/000000/github.png", "https://i.imgur.com/bJE9Pka.png", "https://avatars2.githubusercontent.com/u/25484553?s=200&v=4", "https://img.icons8.com/color/480/000000/heroku.png"];
+
+function populateImages() {
+  let i = 0;
+  let images = document.querySelectorAll(".card img");
+  for (let i = 0; i < imagesArray.length; i++) {
+    images[i].src = imagesArray[i];
+  }
+}
+populateImages();
+
+// Alternative loop:
+// let i = 0; 
+// cards.forEach(card => {
+//   card.src = imagesArray[i];
+//   i++;
+// }) 
+
+// Modification 5.
+function deleteLastCards() {
+  for (let i = 0; i < 3; i++) {
+    let cards = document.querySelectorAll(".col-md-4");
+    let last = (cards.length) - 1;
+    cards[last].remove();
+  }
+}
+deleteLastCards();
+
+// Modificiation 6.
+let newTexts =  ["L’HyperText Markup Language, généralement abrégé HTML, est le langage de balisage conçu pour représenter les pages web", "Les feuilles de style en cascade, généralement appelées CSS de l'anglais Cascading Style Sheets, forment un langage informatique qui décrit la présentation des documents HTML et XML", "JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives mais aussi pour les serveurs. C'est un langage orienté objet à prototype."];
+
+function changeCardsText() {
+  for (let i = 0; i < 3; i++) {
+    let cardTexts = document.querySelectorAll(".card-text");
+    cardTexts[i].textContent = newTexts[i];
+  }
+}
+changeCardsText();
